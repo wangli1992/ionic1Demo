3eec65bc09909de356d31282b210d3b8a9513081
wangli1992 <310894424@qq.com>
2018-05-02 18:09:37 +0800

    test

--- www/page/person/personCtrl.js
+++ www/page/person/personCtrl.js
@@ -3,6 +3,8 @@ angular.module('app.controllers')
 
     $scope.$on("$ionicView.beforeEnter",function () {
       console.log('person page --');
+      $scope.imgArr = [];
+      $scope.imageArr = [];
       $scope.selecteExamSubject = 0;
       $scope.selectExamSubjectIndex = 0;
       $scope.subjectExamDetailArr = [];//初始化学科考试详情数组
@@ -22,65 +24,51 @@ angular.module('app.controllers')
     //选择相册
     $scope.choosePhotos = function(){
       console.log('----点击了 ');
-    //   var imgs=document.getElementsByClassName('imgView');
-    //   var args = {
-    //     'selectMode': 101,//101=PICKER_IMAGE_VIDEO , 100=PICKER_IMAGE , 102=PICKER_VIDEO
-    //     'maxSelectCount': 40, //default 40 (Optional)
-    //     'maxSelectSize': 188743680,//188743680=180M (Optional)
-    //   };
-    //  window.MediaPicker.getMedias(args,function(dataArray){
-    //     //dataArray [{mediaType: "image", path:'/storage/emulated/0/DCIM/Camera/20170808_145202.jpg', size: 21993}]
-    //     console.log('----');
-    //    $scope.getThumbnail(dataArray);
-    // },err());
 
-
-
-      $scope.imgArr = [];
+    
+      var imageData = '';
       window.imagePicker.getPictures(
         function(results) {
-         $state.reload(); 
-         $scope.imgArr = results;
-          // for (var i = 0; i < results.length; i++) {
-          //   var imgData = results[i];
-          //   $scope.imgArr.push(imgData);
-          //   console.log('图片路径是: ' + results[i] );
-
-          // }
-          console.log('输出了。。。' );
+       
+          $scope.imgArr = results;
+          $scope.imageArr = results;
           console.log($scope.imgArr);
           console.log('shuzu长度： ' + $scope.imgArr.length );
+          $scope.$apply();
         }, function (error) {
           console.log('错误信息为: ' + error);
         },{
-          maximumImagesCount: 9
+          maximumImagesCount: 9,
+          width:100,
+          height:100,
+          quality:50
         }
       );
     }
 
-    $scope.getThumbnail = function (dataArray) {
-      for (var i = 0; i < dataArray.length; i++) {
-        MediaPicker.extractThumbnail(dataArray[i], function (data) {
-          imgs[data.index].src = 'data:image/jpeg;base64,' + data.thumbnailBase64;
-          imgs[data.index].setAttribute('style', 'transform:rotate(' + data.exifRotate + 'deg)');
-        }, err());
-      }
-    }
+    // $scope.getThumbnail = function (dataArray) {
+    //   for (var i = 0; i < dataArray.length; i++) {
+    //     MediaPicker.extractThumbnail(dataArray[i], function (data) {
+    //       imgs[data.index].src = 'data:image/jpeg;base64,' + data.thumbnailBase64;
+    //       imgs[data.index].setAttribute('style', 'transform:rotate(' + data.exifRotate + 'deg)');
+    //     }, err());
+    //   }
+    // }
   
-    $scope.convertImgToBase64URL = function(url, callback, outputFormat){
-      var canvas = document.createElement('CANVAS'),
-        ctx = canvas.getContext('2d'),
-        img = new Image();
-      img.crossOrigin = 'Anonymous';
-      img.onload = function () {
-        var dataURL;
-        canvas.height = img.height;
-        canvas.width = img.width;
-        ctx.drawImage(img, 0, 0);
-        dataURL = canvas.toDataURL(outputFormat);
-        callback(dataURL);
-        canvas = null;
-      };
-      img.src = url;
-    }
+    // $scope.convertImgToBase64URL = function(url, callback, outputFormat){
+    //   var canvas = document.createElement('CANVAS'),
+    //     ctx = canvas.getContext('2d'),
+    //     img = new Image();
+    //   img.crossOrigin = 'Anonymous';
+    //   img.onload = function () {
+    //     var dataURL;
+    //     canvas.height = img.height;
+    //     canvas.width = img.width;
+    //     ctx.drawImage(img, 0, 0);
+    //     dataURL = canvas.toDataURL(outputFormat);
+    //     callback(dataURL);
+    //     canvas = null;
+    //   };
+    //   img.src = url;
+    // }
   })
